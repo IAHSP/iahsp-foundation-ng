c031e4ad0fd446b5fb7442bb0c22bb392516b89b
Gabriel Tumbaga <gtumbaga@Masamune.local>
2018-11-04 14:40:51 -0800

    added specific lines that add bootstrap to the NG project, and bootstrap will get webpacked in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AlertModule } from 'ngx-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

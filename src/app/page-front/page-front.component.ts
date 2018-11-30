c86ea3723335900c318a44faf13317e8aa5b7a24
Gabriel Tumbaga <gtumbaga@Masamune.local>
2018-11-29 17:14:00 -0800

    fix the submission forms for check and goods

--- src/app/page-front/page-front.component.ts
+++ src/app/page-front/page-front.component.ts
@@ -7,7 +7,31 @@ import { StaticModalComponent } from './static-modal/static-modal.component';
   styleUrls: ['./page-front.component.scss']
 })
 export class PageFrontComponent implements OnInit {
+  currentFormType = "";
+  modalTitle = "";
 
+  setModalText(whichForm: string) {
+    switch(whichForm) { 
+      case "cc": { 
+        //statements; 
+        this.currentFormType = whichForm;
+        this.modalTitle = "Donate by Credit Card";
+        break; 
+      } 
+      case "ck": { 
+        this.currentFormType = whichForm;
+        this.modalTitle = "Donate by Check";
+        //statements; 
+        break; 
+      } 
+      case "gs": { 
+        //statements; 
+        this.currentFormType = whichForm;
+        this.modalTitle = "Donate by Goods/Services";
+        break; 
+      } 
+    } 
+  }
   constructor() {
     //this.staticModalComponent = new StaticModalComponent;
   }

ada726460d8b2464f4891a0057897f132ca34067
Derrick Leem <derricktleem@gmail.com>
2019-10-10 09:52:43 -0700

    fixed spacing, new donation link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { PageFrontComponent } from './page-front/page-front.component';
+import { SuccessPaymentComponent } from './success-payment/success-payment.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PageFrontComponent
-  },
+  { path: '', component: PageFrontComponent },
+  { path: 'success-payment', component: SuccessPaymentComponent },
 ];
 
 @NgModule({
